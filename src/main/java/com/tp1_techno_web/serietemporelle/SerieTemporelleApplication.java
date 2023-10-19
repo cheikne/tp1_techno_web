a52c1f85996982a8e9f5e43045ad3bd20b3664c2
Cheikne Kane <kane.cheickne@gmail.com>
2023-10-19 09:30:35 +0200

    First commit

---
+++ src/main/java/com/tp1_techno_web/serietemporelle/SerieTemporelleApplication.java
@@ -0,0 +1,13 @@
+package com.tp1_techno_web.serietemporelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerieTemporelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerieTemporelleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp1_techno_web/serietemporelle/SerieTemporelleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp1_techno_web.serietemporelle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerieTemporelleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
