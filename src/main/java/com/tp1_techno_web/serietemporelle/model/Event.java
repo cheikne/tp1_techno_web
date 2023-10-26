921b8e66fbd453da46d0af3c22c919b627ca9364
Cheikne Kane <kane.cheickne@gmail.com>
2023-10-26 09:43:24 +0200

    Adding file Event in the model's repertory

---
+++ src/main/java/com/tp1_techno_web/serietemporelle/model/Event.java
@@ -0,0 +1,62 @@
+package com.tp1_techno_web.serietemporelle.model;
+
+import java.util.Date;
+
+public class Event {
+
+    private long id;
+    private Date date;
+
+    private int valeur;
+    private int tag;
+    private String commentaire;
+
+
+    public Event(long id, Date date, int valeur, int tag, String commentaire) {
+        this.id = id;
+        this.date = date;
+        this.valeur = valeur;
+        this.tag = tag;
+        this.commentaire = commentaire;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+    public int getTag() {
+        return tag;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setValeur(int valeur) {
+        this.valeur = valeur;
+    }
+
+    public void setTag(int tag) {
+        this.tag = tag;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+}
